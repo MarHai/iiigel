ff8645e2738636bdc5a5bfeb26de837d7543773e
Mario Haim <mario@haim.it>
2015-09-20 21:11:26 +0200

    added user group overview for admin/group
    as shown in the skype session

--- src/Controller/Admin/Group.php
+++ src/Controller/Admin/Group.php
@@ -3,6 +3,42 @@
 class Group extends \Iiigel\Controller\Admin\DefaultController {
     const DEFAULT_ACTION = 'showList';
     const TABLE = 'group';
+    
+    /**
+     * Show details for a single entry, based on its hashed ID.
+     * 
+     * @param string $_sHashId hashed representation of ID
+     */
+    public function showDetail($_sHashId) {
+        parent::showDetail($_sHashId);
+        
+        $oSingle = new \Iiigel\Model\GroupAffiliation();
+        $aData = array();
+        $oResult = $oSingle->getList($_sHashId);
+        while(($aRow = $GLOBALS['oDb']->get($oResult))) {
+            $oTemp = new \Iiigel\Model\GroupAffiliation($aRow);
+            $aData[] = $oTemp->getCompleteEntry();
+        }
+        
+        if(count($aData) > 0) {
+            $oTable = new \Iiigel\View\Table($aData);
+            $oTable->addHeadline(ngettext('user2group.member', 'user2group.member.plural', count($aData)));
+
+            $this->oView->addRow();
+            $this->oView->aContent = $oTable->render();
+
+            //von showRights() kopiert - VORLAGE
+            //$oTable->makeInteractive($this->oView);
+            //$oTable->onRowClick(URL.'Admin/Right/showDetail/%s?sPreviousUrl=Admin/'.ucfirst($this::TABLE).'/showDetail/'.$_sHashId);
+
+            //$oForm = new \Iiigel\View\Page();
+            //$oForm->loadTemplate('admin/right.html');
+            //$oForm->aUser = $this->getRightsUserSelection($_sHashId);
+            //$oForm->aTypeId = $this->getRightsTypeIdSelection($_sHashId);
+
+            //$this->oView->aContent = $oTable->render().$oForm->render();
+        }
+    }
 }
 
 ?>
--- src/Model/GroupAffiliation.php
+++ src/Model/GroupAffiliation.php
@@ -2,6 +2,44 @@
 
 class GroupAffiliation extends \Iiigel\Model\Affiliation {
     const TABLE = 'user2group';
+    
+    /**
+     * Load list of all entries, no matter of the current one.
+     * 
+     * @param  string _sHashId hashed ID of group (!)
+     * @return object oDb result object
+     */
+    public function getList($_sHashId = NULL) {
+        return $GLOBALS['oDb']->query('SELECT a.*, b.sName AS sUser, c.sName AS sModule, d.sName AS sChapter
+FROM user2group a, `user` b, module c, chapter d
+WHERE 
+	NOT a.bDeleted 
+	AND a.nIdGroup = (SELECT nId FROM `group` WHERE sHashId = '.$GLOBALS['oDb']->escape($_sHashId).') 
+	AND (a.nStart IS NULL OR a.nStart = 0 OR a.nStart < UNIX_TIMESTAMP()) AND (a.nEnd IS NULL OR a.nEnd = 0 OR a.nEnd > UNIX_TIMESTAMP())
+	AND b.nId = a.nIdUser
+	AND c.nId = a.nIdModule
+	AND d.nId = a.nIdChapter
+ORDER BY sUser ASC');
+    }
+    
+    /**
+     * Runs through all columns (via ->get()) and returns the loaded entry (so far).
+     * 
+     * @param boolean $_bIncludeConfigColumns if true, really all columns are included
+     * @return array   key/value pairing of all columns except CONFIG ones
+     */
+    public function getCompleteEntry($_bIncludeConfigColumns = FALSE) {
+        if($_bIncludeConfigColumns) {
+            return parent::getCompleteEntry(TRUE);
+        } else {
+            return array(
+                'sUser' => $this->sUser,
+                'bAdmin' => $this->bAdmin,
+                'sModule' => $this->sModule,
+                'sChapter' => $this->sChapter
+            );
+        }
+    }
 }
 
 ?>
